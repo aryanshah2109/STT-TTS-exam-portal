d6a2a71b3ac890599439b26811a6f3b437743449
aryan210905 <aryan2109shah@gmail.com>
2026-02-03 20:10:07 +0530

    Question Generation updated

--- backend/fastapi_backend/app/config.py
+++ backend/fastapi_backend/app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
 
     GEMINI_API_KEY: str
     GEMINI_MODEL_NAME: str = "gemini-2.5-flash"
+    HF_EVAL_MODEL_NAME: str = "microsoft/Phi-3.5-mini-instruct"
 
     STT_DEFAULT_MODEL: str = "whisper"
     MCQ_EVAL_MODEL_NAME: str = "sentence-transformers/all-MiniLM-L6-v2"
